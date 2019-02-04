55b8bc5477b88ef5948cbe2145ad57a7da696940
MOLGENIS Jenkins <molgenis+ci@gmail.com>
2019-02-04 12:35:57 +0000

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='molgenis-py-client',
-    version='0.1.0',
+    version='1.0.0',
     description='The Python client for the MOLGENIS REST API',
     url='https://github.com/molgenis/molgenis-py-client/',
     license='GNU Lesser General Public License 3.0',
