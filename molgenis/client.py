eb3347fd43f9d277c2a5aa54e6bb9812e8667065
cpavanrun <C.P.A.vanrun@prinsesmaximacentrum.nl>
2019-07-05 09:28:12 +0200

    Fix HTTP-level authorization errors showing as JSON decoding errors

--- molgenis/client.py
+++ molgenis/client.py
@@ -354,7 +354,10 @@ class Session:
         """Raises an exception with error message from molgenis"""
         message = ex.args[0]
         if ex.response.content:
-            error = json.loads(ex.response.content.decode("utf-8"))['errors'][0]['message']
+            try:
+                error = json.loads(ex.response.content.decode("utf-8"))['errors'][0]['message']
+            except ValueError:  # Cannot parse JSON
+                error = ex.response.content
             error_msg = '{}: {}'.format(message, error)
             raise MolgenisRequestError(error_msg, ex.response)
         else:
